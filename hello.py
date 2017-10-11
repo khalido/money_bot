9652e9b5c12aa0cc3d511d0a46d4619a4c1bb642
KO <khalid.omar@gmail.com>
2017-10-11 17:07:17 +1100

    super basic bot which just sends our msg back to us, running through ngrok of my laptop

--- hello.py
+++ hello.py
@@ -1,5 +1,6 @@
 import requests
-from flask import Flask
+from flask import Flask, request
+import requests
 import configparser
 
 # api keys are in config.ini to keep them out of github
@@ -12,27 +13,53 @@ try:
 except KeyError:
     print("Missing keys in the config.ini file")
 
-
+print(VERIFY_TOKEN)
 
 # the webby stuff, powered by flask
 
 app = Flask(__name__)
 
+# handle verification from fb to authenticate the app
 @app.route('/', methods=['GET'])
 def handle_verification():
     return request.args['hub.challenge']
 
+
+# handle incoming messages and reply to them
+@app.route('/', methods=['POST'])
+def handle_incoming_messages():
+    data = request.json
+    msg = data['entry'][0]['messaging'][0]
+
+    sender_id = msg['sender']['id']         # person sending msg
+    recipient_id = msg["recipient"]["id"]   # our page id
+    message_text = msg['message']['text']   # txt of msg
+
+    print("---printing the data we get from facebook---")
+    print(data)
+
+    reply(sender_id, message_text[::-1])
+    return "ok", 200
+
+def reply(user_id, msg):
+    """takes in user_id and a msg and sends it"""
+    data = {
+        "recipient": {"id": user_id},
+        "message": {"text": msg}
+    }
+
+    post_url = "https://graph.facebook.com/v2.6/me/messages?access_token=" + PAGE_ACCESS_TOKEN
+    resp = requests.post(post_url, json=data)
+    
+    print("---"*10)
+    print(resp.content)
+    print(msg, "msg sent")
+
+# to ab able to eyeball if the server is up and running on the interwebs
 @app.route('/hello')
 def hello():
     return "<h1> Testing this Hello World biz</H1>"
 
-@app.route('/webhook', methods=['POST'])
-def webhook():
-    verify_token_from_fb = request.args.get('hub.verify_token')
-    if VERIFY_TOKEN == verify_token_from_fb:
-        return request.args.get('hub.challenge')
     
-    return "test"
-
 if __name__ == "__main__":
     app.run()
---
+++ temp.py
@@ -0,0 +1,10 @@
+{'object': 'page', 
+'entry': [{'id': '123592025060269', 'time': 1507691690487, 
+'messaging': [{'sender': {'id': '1718968874810833'}, 'recipient': {'id': '123592025060269'}, 'timestamp': 1507691690087, 'message': {'mid': 'mid.$cAABwaE6TmY1lOrLAZ1fCW12ZpMcd', 'seq': 23113, 'text': 'test', 'nlp': {'entities': {'intent': [{'confidence': 1, 'value': 'spend'}]}}}}]
+
+}]
+}
+
+---printing the data we get from facebook---
+{'object': 'page', 'entry': [{'id': '123592025060269', 'time': 1507692075676, 'messaging': [{'sender': {'id': '1718968874810833'}, 'recipient': {'id': '123592025060269'}, 'timestamp': 1507692075290, 'message': {'mid': 'mid.$cAABwaE6TmY1lOrihGlfCXNXHfBFJ', 'seq': 23121, 'text': 'aaaarrr', 'nlp': {'entities': {'intent': [{'confidence': 1, 'value': 'spend'}]}}}}]}]}
+------------------------------
